4b80add3ffa48268e69b30e7cc157c034b5bbdd6
Junge, Gregory (gj8287) <gj8287@us.att.com>
2017-04-07 22:10:27 +0000

    added --user=mysql to mysql_install_db command

--- rootfs/usr/local/bin/mysql_init.sh
+++ rootfs/usr/local/bin/mysql_init.sh
@@ -9,7 +9,7 @@ declare MYSQLD=( $@ )
 
 function mysql_init_install(){
     mkdir -p "$(mysql_datadir)"
-    mysql_install_db --datadir="$(mysql_datadir)" --rpm 
+    mysql_install_db --user=mysql --datadir="$(mysql_datadir)" --rpm 
 }
 
 function mysql_init_start(){
