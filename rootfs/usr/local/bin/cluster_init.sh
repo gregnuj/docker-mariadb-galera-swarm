77e92242ee0604c322c91edb1d1755b350ebb78b
Junge, Gregory (gj8287) <gj8287@us.att.com>
2017-05-23 00:43:12 +0000

    Starting over with cluster settings

--- rootfs/usr/local/bin/cluster_init.sh
+++ rootfs/usr/local/bin/cluster_init.sh
@@ -13,14 +13,14 @@ default_storage_engine = InnoDB
 innodb-doublewrite=1 
 innodb_file_per_table=1
 innodb_autoinc_lock_mode=2
-innodb-flush-log-at-trx-commit=2 
-innodb_log_file_size=48M
+#innodb-flush-log-at-trx-commit=2 
+#innodb_log_file_size=48M
 #auto_increment_increment = 2
 #auto_increment_offset  = 1
 
 # Logs
-log-bin=mysql-bin
-expire-logs-days=2
+#log-bin=mysql-bin
+#expire-logs-days=2
 sync-binlog=1
 binlog-format=row
 
@@ -44,6 +44,26 @@ wsrep_provider_options="pc.recovery=true"
 wsrep_provider_options="pc.wait_prim=true"
 wsrep_provider_options="pc.wait_prim_timeout=PT300S"
 #wsrep_provider_options="pc.weight=$(cluster_weight)"
+
+EOF
+
+cat <<-EOF > "$(cluster_cnf)" 
+wsrep_provider=/usr/lib/libgalera_smm.so
+
+wsrep-cluster-name=$(cluster_name)
+wsrep-cluster-address=$(cluster_address)
+
+#wsrep-node-name=$(fqdn)
+wsrep_node_address=$(node_address)
+
+wsrep_sst_method=$(cluster_sst_method)
+wsrep-sst-auth=$(cluster_sst_auth)
+
+pxc_strict_mode=ENFORCING
+
+binlog_format=ROW
+default_storage_engine=InnoDB
+innodb_autoinc_lock_mode=2
 
 EOF
 
