771bcae697e91d64d30e23b279706cf968d5b419
Junge, Gregory (gj8287) <gj8287@us.att.com>
2017-06-13 04:26:17 +0000

    Modified mysql/galera settings

--- rootfs/usr/local/bin/galera_init.sh
+++ rootfs/usr/local/bin/galera_init.sh
@@ -11,16 +11,24 @@ skip_name_resolve
 
 # InnoDB
 default_storage_engine = InnoDB
-innodb-doublewrite=1 
-innodb_file_per_table=1
 innodb_autoinc_lock_mode=2
+innodb-buffer-pool-size=26G
+innodb_file_per_table=1
 innodb-flush-log-at-trx-commit=0 
-innodb_log_file_size=48M
+innodb-flush-method=O_DIRECT
+innodb_locks_unsafe_for_binlog=1
+innodb-log-file-size=512M
+innodb-log-files-in-group=2
 
 # Logs
-sync-binlog=0
-log-bin=binlog
 binlog-format=row
+log-bin=binlog
+sync-binlog=0
+log_slave_updates
+log-error = /var/log/mysql/mysql-error.log
+log-queries-not-using-indexes=1
+slow-query-log = 1
+slow-query-log-file = /var/log/mysql/mysql-slow.log
 
 # Galera-related settings #
 [galera]
@@ -38,12 +46,12 @@ wsrep-sst-auth=$(wsrep_sst_auth)
 wsrep-provider=/usr/lib/galera/libgalera_smm.so
 wsrep-provider-options="debug=${WSREP_DEBUG}" 
 wsrep_provider_options="evs.keepalive_period = PT3S"
-wsrep_provider_options="evs.suspect_timeout = PT30S"
-wsrep_provider_options="evs.inactive_timeout = PT1M"
-wsrep_provider_options="evs.install_timeout = PT1M"
+wsrep_provider_options="evs.suspect_timeout = P60S"
+wsrep_provider_options="evs.inactive_timeout = PT60S"
+wsrep_provider_options="evs.install_timeout = PT60S"
 wsrep_provider_options="evs.max_install_timeouts=10"
-wsrep_provider_options="evs.user_send_window=512"
-wsrep_provider_options="evs.send_window=1024"
+wsrep_provider_options="evs.user_send_window=1024"
+wsrep_provider_options="evs.send_window=2048"
 wsrep-provider-options="gcache.size=1G" 
 wsrep-provider-options="gcache.page_size=512M" 
 wsrep_provider_options="gcache.recover=yes"
